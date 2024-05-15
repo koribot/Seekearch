8181fb5fa651fb9874f238d40039c101bf63b103
Walid <w.cawasa@gmail.com>
2024-05-15 22:26:06 +0800

    :construction_worker: testing fetching html

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,14 +1,11 @@
 export async function GET(req: any) {
   const search_param = req.nextUrl.searchParams.get("q");
-  const res = await fetch(
-    "https://cors-anywhere.herokuapp.com/" + search_param,
-    {
-      headers: {
-        "Content-Type": "application/json",
-        Origin: "https://seekearch.vercel.app/",
-      },
-    }
-  );
+  const res = await fetch(search_param, {
+    headers: {
+      "Content-Type": "application/json",
+      Origin: "https://seekearch.vercel.app/",
+    },
+  });
   const data = await res.text();
 
   return new Response(data);
