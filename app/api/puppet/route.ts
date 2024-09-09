dcbffcb69f245b907e9976fa5ed845ef0f27fbcf
Walid <w.cawasa@gmail.com>
2024-09-09 20:27:28 +0800

    :construction: added playwright api

--- app/api/puppet/route.ts
+++ app/api/puppet/route.ts
@@ -1,12 +1,17 @@
-import { chromium } from 'playwright';
+import chromium from '@sparticuz/chromium';
+import puppeteerCore from 'puppeteer-core';
 
 export async function GET(req: any) {
   const search_param = req.nextUrl.searchParams.get("q");
   const link = decodeURIComponent(search_param);
   try {
-    const browser = await chromium.launch({ headless: true });
-    const context = await browser.newContext();
-    const page = await context.newPage();
+    const browser = await puppeteerCore.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath(),
+      headless: chromium.headless,
+    });
+    const page = await browser.newPage();
     await page.goto(link);
     const html = await page.content();
     await browser.close();
