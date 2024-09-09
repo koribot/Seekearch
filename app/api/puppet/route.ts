ee1ac40aa9e21565c591faa5091a8474bf88938e
Walid <w.cawasa@gmail.com>
2024-09-09 19:49:37 +0800

    :construction: added puppeteer api

--- app/api/puppet/route.ts
+++ app/api/puppet/route.ts
@@ -5,7 +5,10 @@ export async function GET(req: any) {
   const search_param = req.nextUrl.searchParams.get("q");
   const link = decodeURIComponent(search_param);
   try {
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({ 
+      headless: true ,
+      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
+    });
     const page = await browser.newPage();
     await page.goto(link);
     const html = await page.content();
